cd9c44c51dbb928792e8c59ba9546641f40d2537
dani koskas <danikoskas@MacBook-Pro-de-dani.local>
2018-12-12 14:30:06 +0200

    first commit

---
+++ shop_project/populate_shop_app.py
@@ -0,0 +1,60 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_project.settings')
+import django
+django.setup()
+
+import random
+from faker import Faker
+from shop_app.models import Client, Product
+
+# CONSIGNE
+# creez 2 fonctions :
+#   1. cree 1000 faux clients
+#   2. cree 50 faux produits
+
+fakegen = Faker()
+
+def generate_brand():
+    brands = ["Nike", "Adidas", "Reebook","Jordan","Banlenciaga" ,"asics", "Timberland", "Puma", "Fila", "Geox"]
+    # pk index? 
+    index = random.randint(0, 9)
+    return brands[index]
+
+
+def generate_clients():
+    for client in range(1000):
+        client = Client.objects.get_or_create(first_name=fakegen.first_name(), last_name=fakegen.last_name(), email=fakegen.email(), password=fakegen.password())[0]
+        print(client)
+        # client[0].save()
+
+    
+def generate_products():
+    for product in range(50):
+        price = random.randint(60, 400)
+        # pk tu met brand ds cette def?
+        brand = generate_brand()
+    
+
+        product = Product.objects.get_or_create(name=brand, price=price,description=fakegen.text(max_nb_chars=400))[0]
+        print(product)
+        
+
+
+def populate():
+    # generate_clients()
+    generate_products()
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  print('starting populate...')
+  populate()
+  print('done populating')
+
+
+
---
+++ shop_project/shop_app/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=264)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	description = models.TextField()
+	image = models.ImageField(default='static/images/image.png' , upload_to='static/images/')
+
+
+	def __str__(self):
+		return self.name
+
+	# def __repr__(self):
+	# 	return "<Product {}>".format(self.name)
+
+
+class Client(models.Model):
+	first_name = models.CharField(max_length=264)
+	last_name = models.CharField(max_length=264)
+	email = models.EmailField(max_length=264, unique=True)
+	password = models.CharField(max_length=264)
+	# products = models.ForeignKey(Product, on_delete=models.CASCADE)
+	profile_image = models.ImageField(default='static/images/image.png', upload_to='static/images/profile_pic')
+
+						
+		
+	def __str__(self):
+		return self.first_name
+
+	def __repr__(self):
+		return "<Client {}>".format(self.first_name)
+
+
+class Maillots(models.Model):	
+
+
+
+
+
+
+
+
+
+
+
